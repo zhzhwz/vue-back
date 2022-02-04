59482fe68562a8986371fbb0b7c08343acfe61d4
zhzhwz <954753124@qq.com>
2022-02-04 20:56:55 +0800

    debug

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs')
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.all('/api/users', (request, response) => {
     const users = [
