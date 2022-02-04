6cb8a0d1d1da81d377c68f93761d029d7aef9344
zhzhwz <954753124@qq.com>
2022-02-04 12:41:55 +0800

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const users = [
+    {id: 1, name: 'zhzhwz'},
+    {id: 2, name: 'pipiwu'},
+    {id: 3, name: 'smally'},
+]
+const server = http.createServer(function(request, response) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    if (request.url == '/api/users') {
+        response.end(JSON.stringify(users));
+    }
+    else {
+        response.end('Not Found');
+    }
+});
+server.listen(3000, () => {
+    console.log('Listening 3000');
+});
