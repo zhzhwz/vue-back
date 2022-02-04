e0480c9943f4920f2204d4d24cf939e082f5391a
zhzhwz <954753124@qq.com>
2022-02-04 18:30:58 +0800

    add file apis

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ const express = require('express');
 
 const app = express();
 
-const users = [
-    {id: 1, name: 'zhzhwz'},
-    {id: 2, name: 'pipiwu'},
-    {id: 3, name: 'SmallY'},
-    {id: 4, name: 'Once'},
-];
 app.all('/api/users', (request, response) => {
+    const users = [
+        { id: 1, name: 'zhzhwz' },
+        { id: 2, name: 'pipiwu' },
+        { id: 3, name: 'SmallY' },
+        { id: 4, name: 'Once' },
+    ];
     response.setHeader('Access-Control-Allow-Origin', '*');
-    if (request.url == '/api/users') {
-        response.end(JSON.stringify(users));
-    }
-    else {
-        response.end('Not Found');
-    }
+    response.end(JSON.stringify(users));
 });
+
+app.all('/api/fileName', (request, response) => {
+    const files = [
+        {filename: 'file1'},
+        {filename: 'file2'},
+        {filename: 'file3'},
+    ]
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    request.end(JSON.stringify(files));
+})
 
 app.all('/api/fileDownload', (request, response) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
