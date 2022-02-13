6fd45d5540030a0453e9e915ef5bfcd21d4e58ed
zhzhwz <954753124@qq.com>
2022-02-13 19:02:16 +0800

    add size display

--- src/file/name.js
+++ src/file/name.js
@@ -6,21 +6,22 @@ const { uploadPath } = require('../const.js');
 const router = express.Router();
 
 function getFileName(currentDirPath) {
-    const fileNames = [];
-    fs.readdirSync(currentDirPath).forEach((name) => {
-        const filePath = path.join(currentDirPath, name);
+    const files = [];
+    fs.readdirSync(currentDirPath).forEach((fileName) => {
+        const filePath = path.join(currentDirPath, fileName);
         const stat = fs.statSync(filePath);
         if (stat.isFile()) {
-            fileNames.push(name);
+            const fileSize = stat.size;
+            files.push({ fileName, fileSize });
         }
     });
-    return fileNames;
+    return files;
 }
 
 routerFileName = router.get('/name', (req, res, next) => {
-    const fileNames = getFileName(uploadPath);
+    const files = getFileName(uploadPath);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.end(JSON.stringify({ fileNames }));
+    res.end(JSON.stringify({ files }));
 });
 
 module.exports = routerFileName;
